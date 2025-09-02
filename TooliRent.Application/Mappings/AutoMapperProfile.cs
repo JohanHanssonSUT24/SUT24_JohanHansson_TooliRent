18342a529b8dc9924a21d4fa7d9245ef46cf7967
johan <johan@LAPTOP-6KO4S5LR>
2025-09-02 12:29:18 +0200

    Added base for project

---
+++ TooliRent.Application/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using TooliRent.Application.DTOs;
+using TooliRent.Domain.Entities;
+
+namespace TooliRent.Application.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        { 
+            CreateMap<Tool, ToolDto>().ReverseMap();
+        }
+    }
+}
---
+++ TooliRent.Application/Services/ToolService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using TooliRent.Application.DTOs;
+using TooliRent.Application.Interfaces.Services;
+using TooliRent.Domain.Interfaces.Repositories; 
+
+namespace TooliRent.Application.Services
+{
+    public class ToolService : IToolsService
+    {
+        private readonly IToolRepository _toolRepository;
+        private readonly IMapper _mapper;
+        public ToolService(IToolRepository toolRepository, IMapper mapper)
+        {
+            _toolRepository = toolRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ToolDto>> GetAllToolsAsync()
+        {
+            var tools = await _toolRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ToolDto>>(tools);
+        }
+    }
+}
