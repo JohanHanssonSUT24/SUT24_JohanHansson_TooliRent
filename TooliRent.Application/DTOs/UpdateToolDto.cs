a7bf9ebfaf5e2d9f79b3b5c308ac4783c7ea4c6d
johan <johan@LAPTOP-6KO4S5LR>
2025-09-03 10:20:29 +0200

    Added DTOs

---
+++ TooliRent.Application/DTOs/UpdateToolDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooliRent.Application.DTOs
+{
+    public class UpdateToolDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ToolCategoryId { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
