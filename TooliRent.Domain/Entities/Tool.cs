090501bd37386428a4f5416d3204416e5c6a3e46
johan <johan@LAPTOP-6KO4S5LR>
2025-09-10 12:18:24 +0200

    Added SeedData-file

--- TooliRent.Domain/Entities/Tool.cs
+++ TooliRent.Domain/Entities/Tool.cs
@@ -11,6 +11,7 @@ namespace TooliRent.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public decimal DailyRentalPrice { get; set; }
         public bool IsAvalible { get; set; }
         public bool IsDeleted { get; set; }
 
---
+++ TooliRent.Infrastructure/SeedData/TooliRentSeedData.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TooliRent.Domain.Entities;
+using TooliRent.Infrastructure.Data;
+
+namespace TooliRent.Infrastructure.SeedData
+{
+    public static class TooliRentSeedData
+    {
+        public static async Task EnsureSeedData(TooliRentDbContext context)
+        {
+            if(!await context.Tools.AnyAsync())
+            {
+                var tool1 = new Tool
+                {
+                    Name = "Slagborr",
+                    Description = "Borrmaskin för att borra i betong och sten.",
+                    DailyRentalPrice = 100.00m
+                };
+            }
+        }
+    }
+}
