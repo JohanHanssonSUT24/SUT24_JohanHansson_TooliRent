6c995d99f87691487bc937a08f8b24ae1f69cbda
johan <johan@LAPTOP-6KO4S5LR>
2025-09-12 09:15:46 +0200

    Added tools to seeddata

--- TooliRent.Infrastructure/SeedData/TooliRentSeedData.cs
+++ TooliRent.Infrastructure/SeedData/TooliRentSeedData.cs
@@ -10,14 +10,63 @@ namespace TooliRent.Infrastructure.SeedData
     {
         public static async Task EnsureSeedData(TooliRentDbContext context)
         {
-            if(!await context.Tools.AnyAsync())
+            if(!await context.ToolCategories.AnyAsync())
             {
-                var tool1 = new Tool
+                var powerTools = new ToolCategory { Name = "Powertools" };
+                var regularTools = new ToolCategory { Name = "Regular tools" };
+                await context.ToolCategories.AddRangeAsync(powerTools, regularTools);
+                await context.SaveChangesAsync();
+
+                if( !await context.Tools.AnyAsync())
                 {
-                    Name = "Slagborr",
-                    Description = "Borrmaskin för att borra i betong och sten.",
-                    DailyRentalPrice = 100.00m
-                };
+                    var tools = new List<Tool>
+                    {
+                        new Tool
+                        {
+                            Name = "Slaghammare Bosch",
+                            Description = "Kraftfull borr för sten och cement",
+                            DailyRentalPrice = 100.00m,
+                            ToolCategoryId = powerTools.Id
+                        },
+                        new Tool
+                        {
+                            Name = "Sticksåg DeWalt",
+                            Description = "Sticksåg med utbyttbart blad",
+                            DailyRentalPrice = 99.00m,
+                            ToolCategoryId = powerTools.Id
+                        },
+                        new Tool
+                        {
+                            Name = "Vinkelslip Makita",
+                            Description = "Vinkelslip med 25cm klinga",
+                            DailyRentalPrice = 110.00m,
+                            ToolCategoryId = powerTools.Id
+                        },
+                        new Tool
+                        {
+                            Name = "Hammare",
+                            Description = "Hammare, 11cm, Härdat stål",
+                            DailyRentalPrice = 50.00m,
+                            ToolCategoryId = regularTools.Id
+                        },
+                        new Tool
+                        {
+                            Name = "Såg",
+                            Description = "Fogsvans",
+                            DailyRentalPrice = 45.00m,
+                            ToolCategoryId = regularTools.Id
+                        },
+                        new Tool
+                        {
+                            Name = "Tumstock",
+                            Description = "2m",
+                            DailyRentalPrice = 11.00m,
+                            ToolCategoryId = regularTools.Id
+                        }
+                    };
+                    await context.Tools.AddRangeAsync(tools);
+                    await context.SaveChangesAsync();
+                }
             }
         }
     }
