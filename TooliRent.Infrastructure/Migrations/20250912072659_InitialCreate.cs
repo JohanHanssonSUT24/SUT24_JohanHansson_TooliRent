5b86b477321657b63af4edc9750264cd36e778c8
johan <johan@LAPTOP-6KO4S5LR>
2025-09-12 09:27:43 +0200

    Trying to re-create database

---
+++ TooliRent.Infrastructure/Migrations/20250912072659_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TooliRent.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DailyRentalPrice",
+                table: "Tools",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyRentalPrice",
+                table: "Tools");
+        }
+    }
+}
